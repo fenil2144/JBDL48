fb77ff88f21c479cb32bb85d43cf948e70c40c04
fenil2144 <shahfenil1998@gmail.com>
2023-05-20 19:30:22 +0530

    Session19 Oauth2.0

---
+++ Session19 - OAuth2.0/src/main/java/com/example/SocialApplication.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2012-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SocialApplication extends WebSecurityConfigurerAdapter {
+
+	@RequestMapping("/user")
+	public Map<String, Object> user() {
+		Authentication authentication =  SecurityContextHolder.getContext().getAuthentication();
+		OAuth2User principal = (OAuth2User) authentication.getPrincipal();
+		
+		Map<String, Object> answer = new HashMap<>();
+		answer.put("name", principal.getAttribute("name"));
+		answer.put("company", principal.getAttribute("company"));
+		
+		return answer;
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+		http
+			.authorizeRequests(a -> a
+				.antMatchers("/", "/error", "/webjars/**").permitAll()
+				.anyRequest().authenticated()
+			)
+			.exceptionHandling(e -> e
+				.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+			)
+			.csrf(c -> c
+				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+			)
+			.logout(l -> l
+				.logoutSuccessUrl("/").permitAll()
+			)
+			.oauth2Login();
+		// @formatter:on
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialApplication.class, args);
+	}
+	
+	@RequestMapping("/user_details")
+	public OAuth2User getUserDetails(@AuthenticationPrincipal OAuth2User principal) {
+		Authentication authentication =  SecurityContextHolder.getContext().getAuthentication();
+		System.out.println(authentication.getPrincipal());
+		return principal;
+	}
+	
+	
+
+}
